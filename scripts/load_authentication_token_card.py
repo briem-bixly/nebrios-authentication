9851915a4f3bc09eca46384a5e9bfecf3f0eb147
Brie Marvin <briem@bixly.com>
2015-08-19 15:12:25 -0700

    updated token card

--- scripts/load_authentication_token_card.py
+++ scripts/load_authentication_token_card.py
@@ -1,3 +1,6 @@
+import string
+import random
+
 class load_authentication_token_card(NebriOS):
     listens_to = ['load_authentication_token_card']
 
@@ -5,4 +8,5 @@ class load_authentication_token_card(NebriOS):
         return self.load_authentication_token_card is True
 
     def action(self):
+        self.token = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(36))
         load_card('nebrios-authentication-token')
